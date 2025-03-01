ddd7aae0c2cd3d14f7cf02585199ebc4dd11f25b
‫Sahar Admoni <64500341+saharad1@users.noreply.github.com>
2025-03-01 16:26:51 +0200

    updates

---
+++ src/env_variables.py
@@ -0,0 +1,6 @@
+#  New API key for the Qdrant server
+def get_qdrant_credentials():
+    return (
+        "https://ebfe714e-8eeb-4c5f-82e7-8bf4c55deb12.europe-west3-0.gcp.cloud.qdrant.io:6333",
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3MiOiJtIn0.slf_qR9Fz0WZcrd6ZahDvHwSdHN2Le8uI_mNXL-HdsI",
+    )
