d98e67188a41c27581f03bead3a1ec712b20df58
‫Sahar Admoni <64500341+saharad1@users.noreply.github.com>
2025-03-05 20:47:49 +0200

    document main

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from src.run_travelwise import run_examples, run_pipeline
 
 
 def run_main():
+    """
+    Run the TravelWise Agent pipeline to interactively get trip options based on user input.
+    """
     # run_examples()
     run_pipeline()
 
